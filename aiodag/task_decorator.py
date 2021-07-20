0067e5b56b0fa930bf00109a56eb194fd77d47aa
Artin Sarraf <sarraf.artin@gmail.com>
2021-07-19 20:18:57 -1000

    allow task decorator to be naked decorator or with params, update docs for error handling

--- aiodag/task_decorator.py
+++ aiodag/task_decorator.py
@@ -3,8 +3,7 @@ import inspect
 from functools import wraps
 
 
-# TODO: make this a real decorator with args  e.g. @task(raw=True)
-def task(afunc, *exogenous_deps, raw=False):
+def task(afunc=None, *exogenous_deps, raw=False):
     """
     Example:
 
@@ -26,45 +25,58 @@ def task(afunc, *exogenous_deps, raw=False):
             # b depends on a, but dependency is explicitly stated in task wrapper, not through `val` param
             b = task(B, a)()
 
-    :param afunc: function that creates an awaitable on invocation
+    :param afunc: callable that creates an awaitable on invocation
     :param exogenous_deps: awaitable
+    :param raw: bool
+        if True, will wait on deps rather than gather, and pass task objects
+        to afunc, rather than resolved task results
     :return:
     """
-    @wraps(afunc)
-    def wrapper(*args, **kwargs):
-        async def _inner():
-            callargs = inspect.signature(afunc).bind(*args, **kwargs).arguments
-            if raw:
-                for k, v in callargs.items():
-                    if not inspect.isawaitable(v):
-                        callargs[k] = task_wrapper(v)
-                if callargs:
-                    await asyncio.wait(callargs.values())
-            else:
-                gather_args = {}
-                non_gather_args = {}
-                for k, v in callargs.items():
-                    if inspect.isawaitable(v):
-                        gather_args[k] = v
-                    else:
-                        non_gather_args[k] = v
+    if raw and exogenous_deps:
+        # TODO: if raw=True, provide a way to pass exog deps to task, then can
+        #  have exog deps with raw=True
+        raise ValueError('Cannot have exogenous dependencies on a raw task.')
 
-                gather_args = dict(
-                    zip(
-                        gather_args.keys(),
-                        await asyncio.gather(*gather_args.values())
+    def _decorate(wrapped_afunc):  # this layer is to allow `@task` or `@task(raw=True)`
+        @wraps(wrapped_afunc)
+        def wrapper(*args, **kwargs):
+            async def _inner():
+                callargs = inspect.signature(wrapped_afunc).bind(*args, **kwargs).arguments
+                if raw:
+                    for k, v in callargs.items():
+                        if not inspect.isawaitable(v):
+                            callargs[k] = asyncio.create_task(_task_wrapper(v))
+                    if callargs:
+                        await asyncio.wait(callargs.values())
+                else:
+                    gather_args = {}
+                    non_gather_args = {}
+                    for k, v in callargs.items():
+                        if inspect.isawaitable(v):
+                            gather_args[k] = v
+                        else:
+                            non_gather_args[k] = v
+
+                    gather_args = dict(
+                        zip(
+                            gather_args.keys(),
+                            await asyncio.gather(*gather_args.values())
+                        )
                     )
-                )
-                callargs = {**gather_args, **non_gather_args}
+                    callargs = {**gather_args, **non_gather_args}
 
-            # TODO: if raw=True, need to pass results of exog deps to task somehow...
-            if exogenous_deps:
-                await asyncio.wait(exogenous_deps)
+                if exogenous_deps:
+                    await asyncio.wait(exogenous_deps)
 
-            return await afunc(**callargs)
-        return asyncio.create_task(_inner())
-    return wrapper
+                return await wrapped_afunc(**callargs)
+            return asyncio.create_task(_inner())
+        return wrapper
+
+    if afunc:
+        return _decorate(afunc)
+
+    return _decorate
 
 
-async def task_wrapper(val):
+async def _task_wrapper(val):
     return val
